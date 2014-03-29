c5283dcc91be0c96d750cb2556830867b0d31bd0
shriram <shriramambi@gmail.com>
2014-03-29 00:13:43 -0500

    trans

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('flyr', ['ionic', 'ui.router'])
+angular.module('flyr', ['ionic'])
 
 .run(function($ionicPlatform) {
 	$ionicPlatform.ready(function() {
@@ -13,7 +13,29 @@ angular.module('flyr', ['ionic', 'ui.router'])
 	});
 })
 
-.controller('MainCtrl', function($scope, $ionicSideMenuDelegate) {
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: "/home",
+      templateUrl: "home.html",
+      controller: 'MainCtrl'
+    })
+    .state('infoflyer', {
+      url: "/infoflyer",
+      templateUrl: "infoflyer.html",
+      controller: 'InfoFlyerCtrl'
+    })
+
+    // if none of the above are matched, go to this one
+    $urlRouterProvider.otherwise("/home");
+})
+
+.controller('InfoFlyerCtrl', function($scope) {
+
+})
+
+.controller('MainCtrl', function($scope) {
   // Our controller
   $scope.toggleLeft = function(){
   	$ionicSideMenuDelegate.toggleLeft();
@@ -23,12 +45,12 @@ angular.module('flyr', ['ionic', 'ui.router'])
   {
     id: 1,
     name : "Shriram Sridharan",
-    subject : "PhD prelim"
+    type: 'infoflyer'
   },
   {
     id: 2,
     name : "Sathya Kumaran",
-    subject : "PhD prelim"
+    type : 'infoflyer'
   }];
 
   $scope.doRefresh = function() {
@@ -52,4 +74,4 @@ angular.module('flyr', ['ionic', 'ui.router'])
   	}
   }
   ];
-});
+})
